12d5d9c379f7f9a7c857e0eb11f6e81453c8ec40
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2023-11-16 23:55:47 +0700

    update

--- src/story/story.service.ts
+++ src/story/story.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateStoryDto } from './dto/create-story.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Story, StorySchema } from './schema/story.schema';
+import { Story } from './schema/story.schema';
 import { handleFilter, toSlug } from 'utils/function';
 
 @Injectable()
