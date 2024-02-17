6ff872146271b6aa15a2436172623448dad050f1
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-02-17 10:33:05 +0700

    update

--- src/strategy/jwt.strategy.ts
+++ src/strategy/jwt.strategy.ts
@@ -2,22 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { User } from 'src/user/schema/user.schema';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(
-    configService: ConfigService,
-    @InjectModel(User.name) private userModel: Model<User>,
-  ) {
+  constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: configService.get('SECRET_KEY'),
     });
   }
-  async validate(payload: { _id: string }) {
+  async validate(payload: any) {
     return payload;
   }
 }
