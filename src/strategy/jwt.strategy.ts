80d49a3afb6f15a04d5c546c53a687c736506d36
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-02-20 15:56:14 +0700

    update

--- src/strategy/jwt.strategy.ts
+++ src/strategy/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
 import { PassportStrategy } from '@nestjs/passport';
