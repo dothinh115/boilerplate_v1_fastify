0bc541a72d3bfab217941e3650a67f3f68f3eea1
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-02-20 15:55:26 +0700

    update

--- src/strategy/jwt.strategy.ts
+++ src/strategy/jwt.strategy.ts
@@ -1,17 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
 import { PassportStrategy } from '@nestjs/passport';
+import { Model } from 'mongoose';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/user/schema/user.schema';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(configService: ConfigService) {
+  constructor(
+    configService: ConfigService,
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: configService.get('SECRET_KEY'),
     });
   }
   async validate(payload: any) {
+    const user = await this.userModel.findById(payload._id);
+    if (!user) throw new UnauthorizedException();
     return payload;
   }
 }
