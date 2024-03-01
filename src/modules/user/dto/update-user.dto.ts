662f7caa4818db4dcf600d2003ee88d806063606
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-03-01 16:03:36 +0700

    update

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,14 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { Expose } from 'class-transformer';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @Expose()
-  email?: string;
-  @Expose()
-  password?: string;
-  @Expose()
-  roles?: string;
-  @Expose()
-  actived?: boolean;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
